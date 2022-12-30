64d94b45faee89d0f49b2edfcf6d481f46d9c0dc
Thongzxje <thonghoangdanh@gmail.com>
2022-12-30 21:33:13 +0200

    small add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Download, Features, SectionWrapper } from "./components";
+import assets from "./assets";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        React Native NFT Showcase
-      </h1>
-      <Download />
-      <Features />
-      <SectionWrapper />
-    </div>
+    <>
+      <SectionWrapper
+        title="Your own store of Nifty NFTs. Start selling and growing"
+        description="Buy, store, collect NFTs, exchange & earn crypto. Join 25+ mils people using ProNef Marketplace"
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+    </>
   );
 };
 
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styles from '../styles/Global';
+import assets from '../assets';
 
-const SectionWrapper = () => {
+const SectionWrapper = ({ title, description, showBtn, mockupImg }) => {
   return (
-    <div>SectionWrapper</div>
+    <div className='min-h-screen'>
+      <h1>{title}</h1>
+      <h1>{description}</h1>
+      <img src={mockupImg} alt="mockup" />
+    </div>
   )
 }
 
