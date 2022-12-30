f5d049ee4ab82b858689d844b407324219de5134
Thongzxje <thonghoangdanh@gmail.com>
2022-12-30 21:48:14 +0200

    finishing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,20 @@ const App = () => {
         mockupImg={assets.homeHero}
         banner="banner"
       />
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
     </>
   );
 };
